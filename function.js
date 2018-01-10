eb7ef9845a737b3f096aa804d1c4052a0541bd26
kannavpathania <31707203+kannavpathania@users.noreply.github.com>
2018-01-10 13:15:43 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+function showimage()
+{
+    document.getElementById("kannav").style.display="block";
+}
+function hideimage()
+{
+    document.getElementById("kannav").style.display="none";
+}
+function showiimage()
+{
+    document.getElementById("rupali").style.display="block";
+}
+function hideiimage()
+{
+    document.getElementById("rupali").style.display="none";
+}
